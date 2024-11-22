84d36b042474ebc94692d0af0523b0c42a5aa369
Krystyna Grzesiak <krygrz11@gmail.com>
2024-11-22 21:13:10 +0100

    suppress warnings in python functions #4

--- python/python_imputation_functions.py
+++ python/python_imputation_functions.py
@@ -1,6 +1,8 @@
 from hyperimpute.plugins.imputers import Imputers
+from warnings import filterwarnings
 
 def hyperimpute_imp(X, method):
+    filterwarnings('ignore')
     imputer = Imputers().get(method)
     X_imputed = imputer.fit_transform(X.copy())
     return X_imputed
