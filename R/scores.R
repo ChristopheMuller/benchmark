d45fad7ace40ac19a182c9c24167354cd142bc23
Christophe Muller <mullec@ethz.ch>
2024-12-09 14:40:10 +0100

    add miceDRF

--- R/scores.R
+++ R/scores.R
@@ -7,9 +7,9 @@ scores_for_complete <- function(original_data, amputed_data, imputed_data,
                                                          amputed_data, 
                                                          imputed_data) %>% 
     rename(score = "value")
-  # energy <- as.numeric(miceDRF::energy_dist(X = original_data, 
-  #                                           X_imp = imputed_data))
-  energy <- NULL  ## BUG WITH MICE DRF
+  energy <- as.numeric(miceDRF::energy_dist(X = original_data, 
+                                            X_imp = imputed_data))
+  # energy <- NULL  ## BUG WITH MICE DRF
   # no IScore for complete data
   # ImpScore <- miceDRF::Iscore(X = amputed_data, imputation_func = imputation_fun)
   
@@ -24,9 +24,9 @@ scores_for_incomplete <- function(original_data, imputed_data, imputation_fun) {
   #calculate IScore here
   safe_imputation_fun <- function(missdf) safe_impute(missdf, imputation_fun)
   
-  # ImpScore <- miceDRF::Iscore(original_data, 
-  #                             imputation_func = safe_imputation_fun)
-  ImpScore <- NULL  ## BUG WITH MICE DRF
+  ImpScore <- miceDRF::Iscore(original_data, 
+                              imputation_func = safe_imputation_fun)
+  # ImpScore <- NULL  ## BUG WITH MICE DRF
   data.frame(measure = "IScore", score = as.numeric(ImpScore))
 }
 
