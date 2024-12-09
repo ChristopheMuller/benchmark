fc5ef19c92394e241b5595684ddc1fa4b0c4cd6e
Krystyna Grzesiak <krygrz11@gmail.com>
2024-12-09 20:30:52 +0100

    fixes typo #8

--- R/rrcovNA.R
+++ R/rrcovNA.R
@@ -3,5 +3,5 @@
 impute_impSeq <- function(missdf) 
   rrcovNA::impSeq(missdf)
 
-mpute_impSeqRob <- function(missdf, ...) 
+impute_impSeqRob <- function(missdf, ...) 
   rrcovNA::impSeqRob(missdf, ...)[["x"]]
