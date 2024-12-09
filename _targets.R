c31d860dce25e90568276e04fa0a25d87afbbfba
Christophe Muller <mullec@ethz.ch>
2024-12-09 14:49:32 +0100

    run fast all

--- _targets.R
+++ _targets.R
@@ -41,13 +41,14 @@ path_to_imputed <- "./results/imputed/"
 path_to_methods <- "./data/functions.RDS"
 
 # amputation setup:
-amputation_mechanisms <- c("classic_mar", "dist_shift", "classic_mcar")
-missing_ratios <- c(0.1, 0.3, 0.5)
-amputation_reps <- 3
+# amputation_mechanisms <- c("classic_mar", "dist_shift", "classic_mcar")
+amputation_mechanisms <- c("classic_mar")
+missing_ratios <- c(0.3)
+amputation_reps <- 1
 
 # imputation methods
-imputation_funs <- readRDS(path_to_methods)[c(1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 25)]
-# imputation_funs <- readRDS(path_to_methods)
+# imputation_funs <- readRDS(path_to_methods)[c(1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 25)]
+imputation_funs <- readRDS(path_to_methods)
 
 imputation_methods <- data.frame(method = str_remove(imputation_funs, "impute_"),
                                  imputation_fun = imputation_funs) %>% 
