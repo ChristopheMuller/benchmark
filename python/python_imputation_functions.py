2e348b4fee81c512a69bef31c3a4fbbb24ae84cb
Krystyna Grzesiak <krygrz11@gmail.com>
2024-12-10 15:12:41 +0100

    adds time limit to python functions

--- python/python_imputation_functions.py
+++ python/python_imputation_functions.py
@@ -3,20 +3,23 @@ from warnings import filterwarnings
 from sklearn.experimental import enable_iterative_imputer
 from sklearn.impute import IterativeImputer
 from remasker.remasker_impute import ReMasker
+from timeout_function_decorator import timeout
 
+@timeout(600)
 def hyperimpute_imp(X, method):
     filterwarnings('ignore')
     imputer = Imputers().get(method)
     X_imputed = imputer.fit_transform(X.copy())
     return X_imputed
 
-
+@timeout(600)
 def iterative_imp(X, post):
     filterwarnings('ignore')
     imputer = IterativeImputer(sample_posterior=post)
     X_imputed = imputer.fit_transform(X.copy())
     return X_imputed
 
+@timeout(600)
 def remasker_imp(X):
     filterwarnings('ignore')
     imputer = ReMasker()
