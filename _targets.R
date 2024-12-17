660f12bd9f998df20404d4f69de14a412b5e1159
Christophe Muller <mullec@ethz.ch>
2024-12-17 18:03:07 +0100

    only FEFI debug

--- _targets.R
+++ _targets.R
@@ -50,7 +50,8 @@ amputation_reps <- 5
 imputation_methods <- readRDS(path_to_methods) %>% 
   rename(imputation_fun = `Function name`) %>% 
   mutate(method = str_remove(imputation_fun, "impute_")) %>% 
-  filter(method != "mice_gamlss")
+  filter(method != "mice_gamlss") %>%
+  filter(method == "FEFI")
 
 # parameters:
 params <- create_params(path_to_complete_datasets = path_to_complete_datasets,
