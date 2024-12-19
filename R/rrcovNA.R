63e0b5782998b45a0c207b189c156a49854cce0a
Krystyna Grzesiak <krygrz11@gmail.com>
2024-12-19 12:42:36 +0100

    adds as.data.frame

--- R/rrcovNA.R
+++ R/rrcovNA.R
@@ -7,7 +7,7 @@ impute_impSeqRob <- function(missdf) {
   rownames(missdf) <- 1:nrow(missdf)
   
   # sometimes the output is x and sometimes it's xseq lol
-  imputed <- rrcovNA::impSeqRob(missdf)[["x", exact = FALSE]] 
+  imputed <- as.data.frame(rrcovNA::impSeqRob(missdf)[["x", exact = FALSE]])
   
   imputed <- imputed[ order(as.numeric(row.names(imputed))), ]
   rownames(imputed) <- NULL
