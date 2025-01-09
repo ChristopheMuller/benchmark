20ffabde28edb7b14c153906b34241949cd88240
Krystyna Grzesiak <krygrz11@gmail.com>
2025-01-09 14:29:04 +0100

    returns proper error in fhdi

--- R/FHDI.R
+++ R/FHDI.R
@@ -3,7 +3,7 @@ eval_fhdi_call <- function(missdf, method, ...) {
   capture.output(suppressMessages(
     imputed <- FHDI::FHDI_Driver(missdf, s_op_imputation = method, ...)
   ))
-  
+  if(is.null(imputed)) stop("Internal error. Function returned NULL")
   imputed[["simp.data"]]
 }
 
