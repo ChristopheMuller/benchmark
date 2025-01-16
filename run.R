9e822b43095b382e05637eb12e4f837f71719679
Christophe Muller <mullec@ethz.ch>
2025-01-16 21:36:06 +0100

    64 workers

--- run.R
+++ run.R
@@ -8,7 +8,7 @@
 # targets::tar_make()
 # !!!! python is not working for this plan
 print(Sys.time())
-targets::tar_make_clustermq(workers = 128) # nolintz
+targets::tar_make_clustermq(workers = 64) # nolintz
 print(Sys.time())
 
 
