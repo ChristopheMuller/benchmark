52a2c5039fcfe027e1aa4176d4a0c4283a04efff
Krystyna Grzesiak <krygrz11@gmail.com>
2025-01-20 14:38:08 +0100

    adds code for reading oartial results

---
+++ results/read_partial_results.R
@@ -0,0 +1,25 @@
+
+library(targets)
+library(stringr)
+library(dplyr)
+
+targets_tbl <- tar_progress()
+
+params <- readRDS("./data/params.RDS")
+
+complete_targets <- targets_tbl %>% 
+  filter(progress %in% c("completed", "skipped")) %>% 
+  filter(str_starts(name, "obtained_scores_")) %>% 
+  pull(name)
+
+all_scores <- lapply(complete_targets, function(ith_case) {
+  tar_load(!!ith_case)
+  get(ith_case)
+}) %>% 
+  bind_rows()
+
+params %>% 
+  left_join(all_scores, by = "imputed_id") %>% 
+  dplyr::select(set_id, mechanism, ratio, rep, case, method, imputation_fun, 
+                time, attempts, error, measure, score) %>% 
+  saveRDS("./results/imputation_summary_partial.RDS")
