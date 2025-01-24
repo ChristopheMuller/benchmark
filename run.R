c1975fc54f17f8b25ab16d2a6be048b992423594
Christophe Muller <mullec@ethz.ch>
2025-01-24 15:57:42 +0100

    verbose

--- run.R
+++ run.R
@@ -8,7 +8,7 @@
 # targets::tar_make()
 # !!!! python is not working for this plan
 print(Sys.time())
-targets::tar_make_clustermq(workers = 64) # nolintz
+targets::tar_make_clustermq(workers = 64, reporter="summary") # nolintz
 print(Sys.time())
 
 
