2e1c3dd92512c08e7357edc82d6894ea0181045d
Christophe Muller <mullec@ethz.ch>
2025-02-03 16:36:37 +0100

    update test.sh

--- euler_scripts/test.sh
+++ euler_scripts/test.sh
@@ -5,7 +5,8 @@
 #SBATCH --time=24:00:00
 #SBATCH --cpus-per-task=1
 #SBATCH --job-name=test
-#SBATCH --output=./euler_output/test.out
+#SBATCH --output=$(pwd)/euler_output/test.out
 #SBATCH --mail-type=START,END,FAIL
 
-apptainer exec --bind $HOME:$HOME env.sif "Rscript run.R"
+# Use full paths
+apptainer exec --bind $HOME:$HOME --bind $(pwd):$(pwd) env.sif Rscript $(pwd)/run.R
