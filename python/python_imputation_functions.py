9b72c0e74bb04f5683be667a7fffa339fb625db5
Christophe Muller <mullec@ethz.ch>
2025-02-14 17:14:17 +0100

    add method autocomplete

--- python/python_imputation_functions.py
+++ python/python_imputation_functions.py
@@ -4,6 +4,7 @@ from sklearn.experimental import enable_iterative_imputer
 from sklearn.impute import IterativeImputer
 from remasker.remasker_impute import ReMasker
 from timeout_function_decorator import timeout
+from auto_complete.fit import impute_data as AutoCompleteImpute
 
 
 @timeout(10800)
@@ -27,4 +28,15 @@ def remasker_imp(X, seed):
     imputer = ReMasker()
     imputer.set_seed(seed)
     X_imputed = imputer.fit_transform(X.copy())
+    return X_imputed
+
+@timeout(10800)
+def autocomplete_imp(X, batch_size=2048, epochs=200, 
+                lr=0.1, encoding_ratio=1, depth=1, copymask_amount=0.3, 
+                val_split=0.8, verbose=False, seed=None):
+    filterwarnings('ignore')
+    X_numpy = X.values
+    X_imputed = AutoCompleteImpute(X_numpy, batch_size=batch_size, epochs=epochs, 
+                lr=lr, encoding_ratio=encoding_ratio, depth=depth, copymask_amount=copymask_amount, 
+                val_split=val_split, verbose=verbose, seed=seed)
     return X_imputed
