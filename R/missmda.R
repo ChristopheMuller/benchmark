882545fa3e8d932951fc27462b1b06cc48cb1f2e
Krystyna Grzesiak <krygrz11@gmail.com>
2025-03-25 10:20:35 +0100

    fix missmda methods

--- R/missmda.R
+++ R/missmda.R
@@ -22,22 +22,22 @@ impute_missmda_pca_reg <- function (missdf, ...) {
 
 impute_missmda_MIFAMD_em <- function (missdf, ...) {
   imputed <- missMDA::MIFAMD(X = missdf, method = "EM", nboot = 1)
-  data.frame(imputed[["completeObs"]])
+  data.frame(imputed[[1]][[1]])
 }
 
 impute_missmda_MIFAMD_reg <- function (missdf, ...) {
   imputed <- missMDA::MIFAMD(X = missdf, method = "Regularized", nboot = 1)
-  data.frame(imputed[["completeObs"]])
+  data.frame(imputed[[1]][[1]])
 }
 
 impute_missmda_MIPCA_em <- function (missdf, ...) {
   imputed <- missMDA::MIPCA(X = missdf, method = "EM", nboot = 1)
-  data.frame(imputed[["res.imputePCA"]])
+  data.frame(imputed[[1]][[1]])
 }
 
 impute_missmda_MIPCA_reg <- function (missdf, ...) {
   imputed <- missMDA::MIPCA(X = missdf, method = "Regularized", nboot = 1)
-  data.frame(imputed[["res.imputePCA"]])
+  data.frame(imputed[[1]][[1]])
 }
 
 
