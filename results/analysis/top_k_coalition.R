b1c3d0428820b5498d686818abe7d5af11c6f4fb
KrystynaGrzesiak <krygrz11@gmail.com>
2025-08-04 15:47:11 +0200

    vis

--- results/analysis/top_k_coalition.R
+++ results/analysis/top_k_coalition.R
@@ -2,12 +2,14 @@
 
 library(dplyr)
 
+score_tmp <- "energy_std"
+
 n_methods <- length(unique(imputation_summary$method))
 
 t <- imputation_summary %>%
-  filter(!is.na(measure)) %>% 
+  # filter(!is.na(score)) %>% 
   # select(-imputation_fun) %>%
-  filter(measure == "energy_std") %>% 
+  filter(measure == score_tmp) %>% 
   unique() %>% 
   group_by(method, set_id, mechanism, ratio) %>% 
   reframe(score = mean(score, na.rm = TRUE)) %>% 
@@ -33,7 +35,7 @@ t <- imputation_summary %>%
 total_methods <- unique(imputation_summary$method)
 
 # Define the k values to test
-k_values <- c(1, 2, 3, 4, 5)
+k_values <- 1:5
 
 # Maximum group size to test
 max_group_size <- 10  # Adjust as needed
@@ -105,8 +107,8 @@ final_results <- do.call(rbind, results)
 library(ggplot2)
 library(dplyr)
 
-ggplot(final_results, aes(x = group_size, y = 1 - metric, color = factor(k))) +
-  geom_line(size = 1) +
+p1 <- ggplot(final_results, aes(x = group_size, y = 1 - metric, color = factor(k))) +
+  geom_line(linewidth = 1) +
   geom_point(size = 2) +
   scale_y_continuous(labels = scales::percent_format(accuracy = 1),
                      breaks = 1:10*0.1) +
@@ -123,4 +125,92 @@ ggplot(final_results, aes(x = group_size, y = 1 - metric, color = factor(k))) +
   )
 
 
+library( ggrepel)
 
+final_results %>% 
+  mutate(methods = lapply(strsplit(methods, ", "), function(i) rev(i)[1])) %>% 
+  # mutate(methods = ifelse(group_size > 1, paste0("+ ", methods), methods)) %>% 
+  group_by(k) %>% 
+  mutate(pctg_added = {
+    c((1 - metric)[1], (1 - metric)[-1] - (1 - metric)[-length(metric)])
+  }) %>% 
+  ggplot(aes(x = group_size, y = 1 - metric, color = factor(k))) +
+  geom_line(linewidth = 1) +
+  geom_point(size = 3) +
+  scale_y_continuous(labels = scales::percent_format(accuracy = 1),
+                     breaks = 1:10*0.1) +
+  scale_x_continuous(breaks = 1:max(final_results$group_size)) +
+  labs(
+    title = "",
+    x = "Number of different methods",
+    y = "Proportion of cases in top k ranks",
+    color = "k"
+  ) +
+  theme_minimal(base_size = 15) +
+  theme(
+    plot.title = element_text(hjust = 0.5)
+  ) +
+  # geom_text(aes( x = group_size, y = 1 - metric, color = factor(k), label = methods),
+  #           segment.size  = 0,
+  #           segment.color = "grey50",
+  #           direction     = "y",
+  #           hjust         = 0,
+  #           show.legend = FALSE) +
+  geom_label_repel(aes(x = group_size, y = 1 - metric, fill = factor(k), label = paste0(methods, "  [+", round(pctg_added*100, 0), "%]")),
+             alpha = 0.8, col = "black",
+             segment.size  = 0,
+             segment.color = "grey50",
+             hjust         = 0, show.legend = FALSE,
+             nudge_x = 0.02)  +
+  coord_cartesian(clip = "off") +
+  theme(legend.position = "none"
+  )
+# theme(axis.text.y = element_blank(),
+#       axis.title.y = element_blank())
+
+
+
+# 
+# 
+# final_results %>% 
+#   mutate(methods = lapply(strsplit(methods, ", "), function(i) rev(i)[1])) %>% 
+#   # mutate(methods = ifelse(group_size > 1, paste0("+ ", methods), methods)) %>% 
+#   group_by(k) %>% 
+#   mutate(pctg_added = {
+#     c((1 - metric)[1], (1 - metric)[-1] - (1 - metric)[-length(metric)])
+#   }) %>% 
+#   ggplot(aes(x = k, y = 1 - metric, group = factor(k))) +
+#   geom_line(linewidth = 1) +
+#   geom_point(size = 3) +
+#   scale_y_continuous(labels = scales::percent_format(accuracy = 1),
+#                      breaks = 1:10*0.1) +
+#   scale_x_continuous(breaks = 1:max(final_results$group_size)) +
+#   labs(
+#     title = "",
+#     x = "k",
+#     y = "Proportion of cases in top k ranks",
+#     color = "k"
+#   ) +
+#   theme_minimal() +
+#   theme(
+#     plot.title = element_text(hjust = 0.5)
+#   ) +
+#   # geom_text(aes( x = group_size, y = 1 - metric, color = factor(k), label = methods),
+#   #           segment.size  = 0,
+#   #           segment.color = "grey50",
+#   #           direction     = "y",
+#   #           hjust         = 0,
+#   #           show.legend = FALSE) +http://127.0.0.1:31939/graphics/e79ff87e-06d5-41ec-9f9c-a86a38c797b8.png
+#   geom_label(aes(x = k, y = 1 - metric, label = paste0(methods, "  [+", round(pctg_added*100, 0), "%]")),
+#              alpha = 0.6, col = "black",
+#              segment.size  = 0,
+#              segment.color = "grey50",
+#              hjust         = 0, show.legend = FALSE,
+#              nudge_x = 0.05)  +
+#   coord_cartesian(clip = "off") +
+#   theme(legend.position = "none"
+#   ) +
+#   scale_x_continuous(expand = expansion(mult = c(0, 0.2)))
+# 
+# 
+# 
