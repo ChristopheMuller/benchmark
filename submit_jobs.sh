0494322cf84953698d334ec27ce340b78f7210ef
Christophe Muller <mullerchristophe21@gmail.com>
2026-03-22 16:17:39 +0000

    adjust submission file

--- submit_jobs.sh
+++ submit_jobs.sh
@@ -5,6 +5,6 @@
 while IFS= read -r id; do
   echo "Submitting: $id"
   oarsub -n "impute_${id}" \
-         -l /nodes=1/core=1,walltime=10:00:00 \
-         "Rscript run_one.R ${id}"
+         -l /nodes=1/core=1,walltime=2:00:00 \
+         "Xvfb :99 -screen 0 1024x768x16 & export DISPLAY=:99 && apptainer exec env.sif Rscript run_one.R ${id}"
 done < job_ids.txt
